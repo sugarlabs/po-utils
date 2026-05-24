ec01256bfa66f34318975547a1d28c4e4f9f99ac
Walter Bender <walter@sorceo.com>
2026-05-24 15:07:02 -0400

    add missing newline

--- js2pot.py
+++ js2pot.py
@@ -166,7 +166,7 @@ def mine_js_files(js_pathname, pot_filename):
 
         if js_list[i] in js_trans_dict:
             for j in range(len(js_trans_dict[js_list[i]])):
-                output.write('#.TRANS:%s' % (js_trans_dict[js_list[i]][j]))
+                output.write('#.TRANS:%s\n' % (js_trans_dict[js_list[i]][j]))
 
         # new_phrase = string.replace(js_list[i], '"', '\\"')
         new_phrase = js_list[i].replace('"', '\\"')
